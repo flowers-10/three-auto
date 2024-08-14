23d008bf52bdbe3ec87a99c8093b61c7eb60d96b
Michela <mgaligigi@163.com>
2024-08-14 14:34:41 +0800

    feat:add vite-plugin-dts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import dts from 'vite-plugin-dts'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [dts()],
   build: {
     lib: {
       entry: "core/index.ts", // 工具库入口
