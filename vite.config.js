22a13560c15533c3d328648519605fdb0719452c
KallkaGo <765830637@qq.com>
2024-08-14 11:42:06 +0800

    build: modify config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: "core/index.ts", // 工具库入口
-      name: "auto-three", // 工具库名称
+      name: "three-auto", // 工具库名称
       fileName: (format) => `auto-three.${format}.js`, // 工具库名称
     },
   },
