48bfee0c55e11ba2b3e2018d4ff8e416098f429b
Michela <mgaligigi@163.com>
2024-08-15 10:22:59 +0800

    release:v0.1.2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import dts from 'vite-plugin-dts'
+import {createHtmlPlugin} from "vite-plugin-html"
 
+const HTMLParams = {
+  minify: true,
+  pages:[
+    {
+      fileName: 'index',
+      entry:'/src/main.ts',
+      template: 'index.html'
+    }
+  ]
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts()],
+  plugins: [dts(),createHtmlPlugin(HTMLParams)],
   build: {
     outDir: 'build',
     lib: {
