36afbf976fabbef9d2d3f480f23f871350992624
Michela <mgaligigi@163.com>
2024-08-19 10:19:13 +0800

    test:vite dts issues

--- build/components/sprites/Sprite.d.ts
+++
@@ -1,8 +0,0 @@
-import { ThreeInstance } from '../../base/ThreeInstance';
-import { default as BaseThree } from '../../base/BaseThree';
-import * as THREE from "three";
-export default class Sprite extends BaseThree {
-    spriteGroup: THREE.Group;
-    constructor(instance: ThreeInstance);
-    createSprite(data: any): void;
-}
