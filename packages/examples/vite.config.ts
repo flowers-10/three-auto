32880db86cb7132932474746dea7d3fdd17653b0
Michela <mgaligigi@163.com>
2024-08-30 17:05:21 +0800

    fix:Examples cannot find module 'three-auto' or its corresponding type declarations.

--- packages/examples/vite.config.ts
+++ packages/examples/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import glsl from "vite-plugin-glsl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    glsl({
-      compress: true,
-      watch: true,
-    }),
   ],
 });
