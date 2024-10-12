0c5cbcb8c0d58c8b06daab2442e5ffd6b89bbc6b
KallkaGo <765830637@qq.com>
2024-10-12 12:25:56 +0800

    update test

--- packages/core/main.ts
+++ packages/core/main.ts
@@ -7,9 +7,9 @@ const environmentMap = textureLoader.load('/interior_views_cozy_wood_cabin_with_
 environmentMap.mapping = THREE.EquirectangularReflectionMapping
 environmentMap.colorSpace = THREE.SRGBColorSpace
 const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(
-  256,
+  512,
   {
-      type: THREE.FloatType
+    type: THREE.FloatType
   }
 )
 instance.scene.environment = cubeRenderTarget.texture
@@ -17,20 +17,30 @@ const cubeCamera = new THREE.CubeCamera(0.1, 1000, cubeRenderTarget)
 instance.scene.background = environmentMap
 
 const box = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshStandardMaterial({ roughness: 0.1, metalness: 0.5, color: 0xaaaaaa })
-);
-box.position.x = -2;
-const box2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: '#ef32a1' })
+  new THREE.BoxGeometry(2, 2, 2),
+  new THREE.MeshStandardMaterial({ roughness: 0, metalness: 1, color: 0xaaaaaa })
 );
 
-instance.scene.add(box);
-instance.scene.add(box2);
+box.position.x = -2
+
+const O3D = new THREE.Mesh(
+  new THREE.TorusGeometry(8, 0.5),
+  new THREE.MeshBasicMaterial({ color: 'hotpink' })
+);
+
+const group = new THREE.Group();
+group.add(box);
+
+
+instance.scene.add(group);
+instance.scene.add(O3D);
 
 instance._renderer.setClearColor('#000')
 instance.time.on("tick", () => {
+  group.rotation.y += 0.001
+  box.visible = false
+  cubeCamera.position.copy(box.position)
   cubeCamera.update(instance._renderer, instance.scene)
+  box.visible = true
 });
 
